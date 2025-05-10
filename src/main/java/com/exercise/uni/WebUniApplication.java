f77026a1fa53f6a2508ae4dfd51e00246042fe36
Lenzo99 <39810953+Lenzo99@users.noreply.github.com>
2025-05-10 14:29:54 +0200

    Primo commit. Speriamo bene.

---
+++ src/main/java/com/exercise/uni/WebUniApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.uni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebUniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebUniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercise/uni/WebUniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.uni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebUniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
